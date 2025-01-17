b393e51b239519c860746628f0fd271d6d2b6d90
RichardFelix999 <rickyfelli101@gmail.com>
2025-01-16 22:47:36 -0700

    COM: create_scene

--- 1_create_scene/src/main.js
+++ 1_create_scene/src/main.js
@@ -13,3 +13,10 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
